42bda6e0441115a64d6d24d97dc36f6f3e131a34
Ellis Trickett <ellis.trickett@gmail.com>
2020-04-07 11:15:44 +0100

    Added test for 3 & 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,8 +4,10 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed a multiple of 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
-  
   it 'returns "buzz" when passed a multiple of 5' do
     expect(fizzbuzz(10)).to eq 'buzz'
+  end
+  it 'returns "fizzbuzz" when passed a multiple of 3 and 5' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
 end
